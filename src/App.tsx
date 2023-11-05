dedff497c665c01f61cd08a3f06ac6ef62e569a7
Nathaniyell <essien.nathan@yahoo.com>
2023-11-05 15:19:02 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import './App.css';
 let name:string // How to define a type of string
 name = "Nath"
 //name = 5 //throws an error "Type number is not assignable to a string"
+
+let age:number; //Defines a type of number
+let isStudent: boolean; //Defines a type of boolean
+let hobbies:string[] //the hobbies variable can obly be an array of strings
+
+
+
 function App() {
   return (
     <div className="App">
