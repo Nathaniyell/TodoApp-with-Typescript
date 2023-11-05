1af29477b98da622e53aec1fe5098cef7e1eeaf7
Nathaniyell <essien.nathan@yahoo.com>
2023-11-05 16:49:39 +0100

    update InputField.tsx and styles.css

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,7 @@
 import "./styles.css"
 
 const InputField = ()=>{
-return <form action="input">
+return <form className="input">
     <input type="input" placeholder="Enter a task" className="input__box" />
     <button className="input__submit" type="submit">Go</button>
 </form>
