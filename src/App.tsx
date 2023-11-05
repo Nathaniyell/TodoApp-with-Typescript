06aa0dcefaf9c9a311817e1a9f19e1b2442c57a9
Nathaniyell <essien.nathan@yahoo.com>
2023-11-05 18:27:57 +0100

    update App.tsx and InputField.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
 import "./App.css"
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App:React.FC = () =>{ //React.FC specifies a functional component
 
 const [todo, setTodo] = useState<string>("") //the angle brackets is used to define the type of the todo variable, the union(|) symbol can be used to specify multiple types: string|number etc
 
+const [todos, setTodos] = useState<Todo[]>([]) 
+
+const handleAdd=(e:React.FormEvent)=>{
+  e.preventDefault()
+  if(todo){
+    setTodos([...todos, {id: Date.now(), isDone: false, todo:todo }])
+    setTodo("")
+  }
+  console.log(todos);
+}
 
   return <div className="App">
 <span className="heading">
   Taskify
 </span>
 
-<InputField todo={todo} setTodo={setTodo} />
+<InputField todo={todo} setTodo={setTodo} 
+handleAdd={handleAdd}
+/>
   </div>;
 }
 
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,19 +2,19 @@ import "./styles.css";
 
 type Props = {
   todo: string;
-  setTodo: React.Dispatch<React.SetStateAction<string>>; //This is gotten from 
+  setTodo: React.Dispatch<React.SetStateAction<string>>; //This is gotten from
+  handleAdd: (e:React.FormEvent) => void;
 };
 
-const InputField:React.FC<Props> = ({ todo, setTodo }) => {
-
-
-
-
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box"
-      value={todo}
-      onChange={(e)=>setTodo(e.target.value)}
+    <form className="input" onSubmit={handleAdd}>
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button className="input__submit" type="submit">
         Go
