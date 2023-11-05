c85215b052727fce4d65fa743492bc7beb839bbb
Nathaniyell <essien.nathan@yahoo.com>
2023-11-05 15:34:03 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
-let name:string // How to define a type of string
-name = "Nath"
+let name: string; // How to define a type of string
+name = "Nath";
 //name = 5 //throws an error "Type number is not assignable to a string"
 
-let age:number; //Defines a type of number
+let age: number; //Defines a type of number
 let isStudent: boolean; //Defines a type of boolean
-let hobbies:string[] //the hobbies variable can only be an array of strings
-let arrayOfNumbers:number[] //the arrayOfNumbers variable can only contain numbers
-let role:[number, string]; //A tuple: which Implies that the variable can only contain one number and one string
+let hobbies: string[]; //the hobbies variable can only be an array of strings
+let arrayOfNumbers: number[]; //the arrayOfNumbers variable can only contain numbers
+let role: [number, string]; //A tuple: which Implies that the variable can only contain one number and one string
 // Initialize it
 //role = ["hello", 10]; // Error
 // Initialize it incorrectly
 role = [10, "hello"]; // OK
 
+//Defining types for an object
+type Person = {
+  name: string;
+  age: number;
+};
 
+let person: Person; //Attaches the properties of the Person object to this person variable
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
