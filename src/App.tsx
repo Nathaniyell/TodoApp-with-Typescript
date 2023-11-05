2664d20ffe307e7d335062754418fa8a6aa4fc35
Nathaniyell <essien.nathan@yahoo.com>
2023-11-05 15:16:19 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import './App.css';
 
+let name:string = "Nath" // How to define a type of string
+//name = 5 //throws an error "Type number is not assignable to a string"
 function App() {
-  let name = "Nath"
   return (
     <div className="App">
       Hello World
