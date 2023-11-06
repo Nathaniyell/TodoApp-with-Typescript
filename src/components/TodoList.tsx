cd227e19c05f0737ba3929e72fba5cb47cd87e7c
Nathaniyell <essien.nathan@yahoo.com>
2023-11-06 12:53:37 +0100

    update SingleTodo.tsx, TodoList.tsx and styles.css

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,6 @@
 import { Todo } from "../model";
 import SingleTodo from "./SingleTodo";
-
+import "./styles.css"
 type Props = {
     todos: Todo[],
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
