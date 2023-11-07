a17b4a2b9cba2188928fe7e106ee877e2cd742c1
Nathaniyell <essien.nathan@yahoo.com>
2023-11-07 21:28:41 +0100

    add function to handle delete functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -18,11 +18,15 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
       )
     );
   };
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   //s tag is used to cross off a piece of text
   return (
     <form className="todos__single">
       {todo.isDone ? (
-        <s className="todos__single--text">{todo.todo}</s> 
+        <s className="todos__single--text">{todo.todo}</s>
       ) : (
         <span className="todos__single--text">{todo.todo}</span>
       )}
@@ -31,7 +35,7 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
         <span className="icon">
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
         <span className="icon" onClick={() => handleDone(todo.id)}>
