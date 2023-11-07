46bd54bc25ea39b2afc74353461d7e5653d33ea9
Nathaniyell <essien.nathan@yahoo.com>
2023-11-07 21:13:46 +0100

    update SingleTodo.tsx

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
-import "./styles.css"
+import "./styles.css";
 
 type Props = {
   todo: Todo;
@@ -11,14 +11,17 @@ type Props = {
 };
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
-
-  const handleDone=(id:number)=>{
-    setTodos(todos.map(todo=>todo.id===id?{...todo, isDone:!todo.isDone}: todo))
-
-  }
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
   return (
     <form className="todos__single">
-      <span className="todos__single--text">{todo.todo}</span>
+      {!todo.isDone && <span className="todos__single--text">{todo.todo}</span>}
+
       <div>
         <span className="icon">
           <AiFillEdit />
@@ -26,7 +29,7 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
         <span className="icon">
           <AiFillDelete />
         </span>
-        <span className="icon" onClick={()=>handleDone(todo.id)}>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
