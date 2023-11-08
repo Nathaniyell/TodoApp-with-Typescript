3d9704ad1a0a0b2c3be8fa5314caa2a7e7f28a21
Nathaniyell <essien.nathan@yahoo.com>
2023-11-08 05:42:50 +0100

    update SingleTodo.tsx

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -13,6 +13,7 @@ type Props = {
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
   const [edit, setEdit] = useState<boolean>(false);
   const [editTodo, setEditTodo] = useState<string>(todo.todo);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleDone = (id: number) => {
     setTodos(
@@ -25,37 +26,38 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-  const handleEdit = (e:React.FormEvent, id: number) => {
-e.preventDefault()
-setTodos(
-  todos.map(todo=>todo.id===id?{...todo, todo:editTodo}: todo)
-)
-setEdit(false)
-  }
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
   //s tag is used to cross off a piece of text
   return (
-    <form className="todos__single" onSubmit={(e)=>handleEdit(e, todo.id)}>
-{
-  edit ?(
-<input type="text" value={editTodo} onChange={(e)=>setEditTodo(e.target.value)} className="todos__single--test" />
-  ): (
-
-    
-          todo.isDone ? (
-            <s className="todos__single--text">{todo.todo}</s>
-          ) : (
-            <span className="todos__single--text">{todo.todo}</span>
-          )
-  )
-}
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          type="text"
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos__single--test"
+        />
+      ) : todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
 
       <div>
-        <span className="icon" onClick={()=>{
-          if(!edit && !todo.isDone){
-            setEdit(!edit)
-          }
-        }
-      }
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
         >
           <AiFillEdit />
         </span>
