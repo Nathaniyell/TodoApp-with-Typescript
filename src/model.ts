facd906aed9e3dac4cfc7c2215943d65fca9ff7b
Nathaniyell <essien.nathan@yahoo.com>
2023-11-11 21:58:49 +0100

    update SingleTodo.tsx, styles.css and model.ts

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,18 @@
+import {useReducer} from "react"
 export type Todo = {
     id: number;
     todo: string;
     isDone:boolean;
+}
+type Actions =
+|{    type: "add", payload:string}
+|{    type: "remove", payload:number}
+|{    type: "add", payload:string}
+
+const reducer = (state:Todo[], action:)=>{
+
+}
+
+const TodoReducer = ()=>{
+    const [state, dispatch] = useReducer(reducer, [])
 }
