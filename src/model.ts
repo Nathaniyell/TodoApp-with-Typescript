9f663ba69066fa5673dc164b883098c4c3f018f6
Nathaniyell <essien.nathan@yahoo.com>
2023-11-11 22:01:33 +0100

    update model.ts

--- src/model.ts
+++ src/model.ts
@@ -7,10 +7,14 @@ export type Todo = {
 type Actions =
 |{    type: "add", payload:string}
 |{    type: "remove", payload:number}
-|{    type: "add", payload:string}
+|{    type: "done", payload:number}
 
-const reducer = (state:Todo[], action:)=>{
-
+const reducer = (state:Todo[], action:Actions)=>{
+switch(action.type){
+    case "add":
+        return[...state, {id:Date.now(), todo:action.payload, isDone:false}];
+        case "remove":
+}
 }
 
 const TodoReducer = ()=>{
