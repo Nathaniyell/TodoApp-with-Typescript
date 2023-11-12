2cd3179d5f990cdcbd2d6c526405737a1098ea29
Nathaniyell <essien.nathan@yahoo.com>
2023-11-12 20:32:57 +0100

    update model.ts

--- src/model.ts
+++ src/model.ts
@@ -13,8 +13,12 @@ const reducer = (state:Todo[], action:Actions)=>{
 switch(action.type){
     case "add":
         return[...state, {id:Date.now(), todo:action.payload, isDone:false}];
+        break;
         case "remove":
-            return state.filter(todo=>todo.id !==action.payload)
+            return state.filter(todo=>todo.id !==action.payload);
+            break;
+            case "done":
+                return state.map(todo=> todo.id!==action.payload ? {...todo, isDone:!todo.isDone }: todo)
         
 }
 }
